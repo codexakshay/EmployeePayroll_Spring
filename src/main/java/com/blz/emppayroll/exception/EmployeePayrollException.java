e4ae73340bf09a1207a7f03d77f48d4aa6208d80
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-12-23 08:56:01 +0530

    Added : Throw User Friendly Errors For Id Not Found

---
+++ src/main/java/com/blz/emppayroll/exception/EmployeePayrollException.java
@@ -0,0 +1,7 @@
+package com.blz.emppayroll.exception;
+
+public class EmployeePayrollException extends RuntimeException {
+	public EmployeePayrollException(String message) {
+		super(message);
+	}
+}
--- src/main/java/com/blz/emppayroll/service/EmployeePayrollService.java
+++ src/main/java/com/blz/emppayroll/service/EmployeePayrollService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.blz.emppayroll.dto.EmployeePayrollDTO;
+import com.blz.emppayroll.exception.EmployeePayrollException;
 import com.blz.emppayroll.model.EmployeePayrollData;
 
 @Service
@@ -18,7 +19,10 @@ public class EmployeePayrollService implements IEmployeePayrollService{
 	}
 
 	public EmployeePayrollData getEmployeePayrollDataById(int empId) {
-		return employeePayrollList.get(empId-1);
+		return employeePayrollList.stream()
+					.filter(empData -> empData.getEmployeeId() == empId)
+					.findFirst()
+					.orElseThrow(() -> new EmployeePayrollException("Employee Not Found"));
 	}
 
 	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO) {
